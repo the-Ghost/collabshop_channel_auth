7154988cf4f26f5f641657cc117c85ef6cb64087
Kinshuk Bairagi <me@kinshuk.in>
2014-08-08 01:57:28 +0530

    send

---
+++ send.php
@@ -0,0 +1,11 @@
+<?php 
+
+include("lib/Pusher.php");
+
+$pusher = new Pusher('0f12f4175f151876a520', '889319b2023b436cdecf', '84616');
+
+$channel = $_REQUEST['channel'];
+$event = $_REQUEST['event'];
+$payload = $_REQUEST['payload'];
+
+$pusher->trigger('my-channel', 'my-event', json_decode($payload));
