db57576dd98d560909a81e377399c226ce4c5a54
Kinshuk Bairagi <me@kinshuk.in>
2014-08-08 05:44:28 +0530

    update payload

--- send.php
+++ send.php
@@ -6,9 +6,17 @@ $pusher = new Pusher('0f12f4175f151876a520', '889319b2023b436cdecf', '84616');
 
 $channel = $_REQUEST['channel'];
 $event = $_REQUEST['event'];
-$payload = $_REQUEST['payload'];
 
+$meta = $_REQUEST['meta'];
+$action = $_REQUEST['action'];
+$pageType = $_REQUEST['pageType'];
 
-$pusher->trigger($channel , $event , json_decode($payload));
+$payload = array( 
+	"meta" => json_decode($meta) ,
+	"action" => json_decode($action) , 
+	"pageType" => json_decode($pageType)
+	);
+
+$pusher->trigger($channel , $event , $payload);
 
 echo '{ "status" : "send" }';
