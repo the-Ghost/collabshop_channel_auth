39112f29b66618992aaf19b71346ab18361aab08
Kinshuk Bairagi <me@kinshuk.in>
2014-08-08 13:36:02 +0530

    send change to xml

--- send.php
+++ send.php
@@ -7,19 +7,20 @@ $pusher = new Pusher('0f12f4175f151876a520', '889319b2023b436cdecf', '84616');
 $channel = $_REQUEST['channel'];
 $event = $_REQUEST['event'];
 
-$meta = $_REQUEST['meta'];
-$action = $_REQUEST['action'];
-$pageType = $_REQUEST['pageType'];
-$bundle = $_REQUEST['bundle'];
+// $meta = $_REQUEST['meta'];
+// $action = $_REQUEST['action'];
+// $pageType = $_REQUEST['pageType'];
+// $bundle = $_REQUEST['bundle'];
+$payload = $_REQUEST['payload'];
 
-$payload = array( 
-	"channel" => $channel ,
-	"event" => $event ,
-	"meta" => json_decode($meta) ,
-	"action" => json_decode($action) , 
-	"pageType" => json_decode($pageType),
-	"bundle" => json_decode($bundle)
-	);
+// $payload = array( 
+// 	"channel" => $channel ,
+// 	"event" => $event ,
+// 	"meta" => json_decode($meta) ,
+// 	"action" => json_decode($action) , 
+// 	"pageType" => json_decode($pageType),
+// 	"bundle" => json_decode($bundle)
+// 	);
 
 $pusher->trigger($channel , $event , $payload);
 
